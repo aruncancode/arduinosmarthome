a78197472fbe8aa2626f51371c4a46d735bdb9da
Arun Arjunan <arun.arjunan1@outlook.com>
2020-01-20 13:57:45 +0800

    Initial Commit

---
+++ ESP.py
@@ -0,0 +1,46 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+class ESP:
+    def __init__(self,name,ip):
+        self.name=name
+        self.ip = ip
+        self.components = {}
+        self.status = {}
+
+    def return_value(self,url,key):
+        state = requests.get(url)
+        state = BeautifulSoup(state.text, 'html.parser').get_text()
+        json_acceptable_string1 = state.replace("'", "\"")
+        state = json.loads(json_acceptable_string1)
+        state = state[key]
+        return state
+    
+    def update_status(self, component, state):
+            self.status[component] = state
+
+    def request_page(self,component, state):
+        try:
+            pin = self.components[component]
+            requests.get(self.ip + "digital/" + pin + "/" + state)
+            self.update_status(component, state)
+        except: 
+            ConnectionError
+    
+    
+    def esp_status(self):
+        try:
+            status = self.return_value(self.ip, 'connected')
+            return status
+        except:
+            ConnectionError
+   
+    def boot_status(self, component):
+        pin = self.components[component]
+        try:
+            status = self.return_value(self.ip + 'digital/' + pin +"/r", 'return_value')
+            self.update_status(component, status)
+            return status
+        except:
+            ConnectionError
+    
---
+++ Mainserver.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from ESP import *
+
+
+app = Flask(__name__)
+
+esp1 = ESP("esp1", "http://192.168.0.21/")
+esp1.components = {"LED1": "5", "LED2": "6"}
+
+esp2 = ESP("esp2", "http://192.168.0.22/")
+esp2.components = {"LED3": "5", "LED4": "6"}
+
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    if request.method == 'POST':
+        if request.form.get('LED1'):
+            esp1.request_page('LED1', request.form['LED1'])
+        elif request.form.get('LED2'):
+            esp1.request_page('LED2', request.form['LED2'])
+        elif request.form.get('LED3'):
+            esp2.request_page('LED3', request.form['LED3'])
+        elif request.form.get('LED4'):
+            esp2.request_page('LED4', request.form['LED4'])
+    return render_template("gui.html", led1=esp1.boot_status('LED1'), led2=esp1.boot_status('LED2'), 
+    led3=esp2.boot_status('LED3'), led4=esp2.boot_status('LED4'), esp1status=esp1.esp_status(), esp2status=esp2.esp_status())
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=1000)
